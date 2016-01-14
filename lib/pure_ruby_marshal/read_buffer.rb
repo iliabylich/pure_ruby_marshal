fde2dc8bd549dd2de8d1ec2a046985b5048671c1
Ilya Bylich <ibylich@gmail.com>
2016-01-14 17:13:01 +0300

    Added support for integers.

--- lib/pure_ruby_marshal/read_buffer.rb
+++ lib/pure_ruby_marshal/read_buffer.rb
@@ -21,8 +21,45 @@ class PureRubyMarshal::ReadBuffer
     when '0' then nil
     when 'T' then true
     when 'F' then false
+    when 'i' then read_integer
     else
       raise NotImplementedError, "Unknown object type #{char}"
+    end
+  end
+
+  def read_integer
+    # c is our first byte
+    c = (read_byte ^ 128) - 128
+
+    if c == 0
+      # 0 means 0
+      0
+    elsif c > 0
+      if 4 < c && c < 128
+        # case for small numbers
+        c - 5
+      else
+        # otherwise c next bytes is our number
+        c.
+          times.
+          map { |i| [i, read_byte] }.
+          inject(0) { |result, (i, byte)| result | (byte << (8*i))  }
+      end
+    else
+      if -129 < c && c < -4
+        # case for small numbers
+        c + 5
+      else
+        # (-c) next bytes os our number
+        (-c).
+          times.
+          map { |i| [i, read_byte] }.
+          inject(-1) do |result, (i, byte)|
+            a = ~(0xff << (8*i))
+            b = byte << (8*i)
+            (result & a) | b
+          end
+      end
     end
   end
 end
--- spec/fixtures.rb
+++ spec/fixtures.rb
@@ -1,5 +1,10 @@
 FIXTURES = {
   'nil' => nil,
   'true' => true,
-  'false' => false
+  'false' => false,
+  'zero' => 0,
+  'small positive number' => 10,
+  'small negative number' => -10,
+  'big positive number' => 99999,
+  'big negative number' => -99999,
 }
